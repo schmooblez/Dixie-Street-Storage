d9e087f30b4587d3f69cfc4598d1c00e3720bc8b
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-19 00:20:50 -0500

    Like to home page is absolute /

--- includes/_navigation.php
+++ includes/_navigation.php
@@ -2,9 +2,9 @@
     <h1 class="text-center">
         <span class="company-name">Dixie Street Storage</span><br/><span class="company-type">Family Owned and Operated</span></h1>
 </div>
-<nav class="container">
+<nav id="main-nav" class="container">
     <ul class="nav nav-justified">
-        <li><a href="/index.php">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/storagetips.php">Storage Tips</a></li>
         <li><a href="/sizeguide.php">Size Guide</a></li>
         <li><a href="/faq.php">FAQ</a></li>
