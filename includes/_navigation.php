f9080c2028715bb5ff9c31933c73968c9de91223
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-18 23:14:30 -0500

    Updating anchor URIs from .html to .php

--- includes/_navigation.php
+++ includes/_navigation.php
@@ -1,24 +1,24 @@
-<a href="index.html"><div class="header">
+<a href="/index.html"><div class="header">
 	<h1 class= "logo" id="compname">Dixie Street Storage</h1>
 	<h1 class="logo" id="tagline">Family Owned and Operated</h1>
 </div></a>
 <!--This menu will show at the top for phones only-->
 <div class="navphone">
 		<ul>
-			<a href="index.html"><li id="home2">Home</li></a>
-			<a href="storagetips.html"><li id="storagetips2">Storage Tips</li></a>
-			<a href="sizeguide.html"><li id="sizeguide2">Size Guide</li></a>
-			<a href="faq.html"><li id="faq2">FAQ</li></a>
-			<a href="contact.php"><li id="contact2">Contact</li></a>
+			<a href="/index.php"><li id="home2">Home</li></a>
+			<a href="/storagetips.php"><li id="storagetips2">Storage Tips</li></a>
+			<a href="/sizeguide.php"><li id="sizeguide2">Size Guide</li></a>
+			<a href="/faq.php"><li id="faq2">FAQ</li></a>
+			<a href="/contact.php"><li id="contact2">Contact</li></a>
 		</ul>
 </div>
 <!--This menu will show at the top for non-mobile browsers only-->
 <nav class="nav1">
 	<ul>
-		<li id="contact1"><a href="contact.php">Contact</a></li>
-		<li id="faq1"><a href="faq.html">FAQ</a></li>
-		<li id="sizeguide1"><a href="sizeguide.html">Size Guide</a></li>
-		<li id="storagetips1"><a href="storagetips.html">Storage Tips</a></li>
-		<li id="home1"><a href="index.html">Home</a></li>
+		<li id="contact1"><a href="/contact.php">Contact</a></li>
+		<li id="faq1"><a href="/faq.php">FAQ</a></li>
+		<li id="sizeguide1"><a href="/sizeguide.php">Size Guide</a></li>
+		<li id="storagetips1"><a href="/storagetips.php">Storage Tips</a></li>
+		<li id="home1"><a href="/index.php">Home</a></li>
 	</ul>
 </nav>
