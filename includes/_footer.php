792911265cd75371866a6ca0a31322724843f281
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-19 00:04:54 -0500

    Bootstrapping the footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,15 +1,19 @@
 <!--Footer at the bottom with navigation links and copyright-->
-<div class="footer">
-    <nav id ="footnav">
-        <a href="index.html">Home</a>
-        <a href="storagetips.html">Storage Tips</a>
-        <a href="sizeguide.html">Size Guide</a>
-        <a href="faq.html">FAQ</a>
-        <a href="contactus.php">Contact</a>
-    </nav>
-    <p id="copy"> &copy; Copyright 2014 Dixie Street Storage. </p>
-</div>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<nav id ="footnav">
+    <div class="navbar-header">
+        <p class="navbar-brand">&copy; Copyright 2014 Dixie Street Storage.</p>
+    </div>
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav pull-right">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="storagetips.html">Storage Tips</a></li>
+            <li><a href="sizeguide.html">Size Guide</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+            <li><a href="contactus.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+</div<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 <script type="text/javascript">
 if (typeof jQuery == 'undefined') {
     document.write(unescape("%3Cscript src='/js/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));
