91211bbcce1aa273ed0bbe9da8c86abd788f8319
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-18 23:15:32 -0500

    Correcting stylesheet path
    /styles.css => /css/styles.css

--- includes/_head.php
+++ includes/_head.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-	<head>
-	<link rel="stylesheet" type="text/css" href="/cssreset.css">
-	<link rel="stylesheet" type="text/css" href="/cssforall.css">
-	<link rel="stylesheet" type="text/css" href="/cssindexonly.css">
-	<title><?php echo $title; ?></title>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/css/cssreset.css">
+    <link rel="stylesheet" type="text/css" href="/css/cssforall.css">
+    <link rel="stylesheet" type="text/css" href="/css/cssindexonly.css">
+    <title><?php echo $title; ?></title>
 </head>
