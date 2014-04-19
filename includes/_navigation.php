2bab3e88e0607f5383d277afe35b8b7834b26e51
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-19 00:05:32 -0500

    Bootstrapping Site-Wide navigation

--- includes/_navigation.php
+++ includes/_navigation.php
@@ -1,12 +1,13 @@
 <div class="page-header">
-	<h1>Dixie Street Storage<br/>Family Owned and Operated</h1>
+    <h1 class="text-center">
+        <span class="company-name">Dixie Street Storage</span><br/><span class="company-type">Family Owned and Operated</span></h1>
 </div>
 <nav class="container">
-	<ul class="nav nav-justified">
-		<li><a href="/index.php">Home</a></li>
-		<li><a href="/storagetips.php">Storage Tips</a></li>
-		<li><a href="/sizeguide.php">Size Guide</a></li>
-		<li><a href="/faq.php">FAQ</a></li>
-		<li><a href="/contact.php">Contact</a></li>
-	</ul>
+    <ul class="nav nav-justified">
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/storagetips.php">Storage Tips</a></li>
+        <li><a href="/sizeguide.php">Size Guide</a></li>
+        <li><a href="/faq.php">FAQ</a></li>
+        <li><a href="/contact.php">Contact</a></li>
+    </ul>
 </nav>
