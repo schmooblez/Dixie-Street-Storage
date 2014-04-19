a2b53ba164f9dc3bec0c465d55f92b0ad421f987
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-18 23:23:25 -0500

    Adding resource includes for jQuery and Bootstrap
    jQuery has a self-hosted fallback if the Google CDN fails. Explore using
    a Bootstrap fallback in future releases.

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,11 +1,18 @@
 <!--Footer at the bottom with navigation links and copyright-->
 <div class="footer">
-	<nav id ="footnav">
-		<a href="index.html">Home</a>
-		<a href="storagetips.html">Storage Tips</a>
-		<a href="sizeguide.html">Size Guide</a>
-		<a href="faq.html">FAQ</a>
-		<a href="contactus.php">Contact</a>
-	</nav>
-	<p id="copy"> &copy; Copyright 2014 Dixie Street Storage. </p>
+    <nav id ="footnav">
+        <a href="index.html">Home</a>
+        <a href="storagetips.html">Storage Tips</a>
+        <a href="sizeguide.html">Size Guide</a>
+        <a href="faq.html">FAQ</a>
+        <a href="contactus.php">Contact</a>
+    </nav>
+    <p id="copy"> &copy; Copyright 2014 Dixie Street Storage. </p>
 </div>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<script type="text/javascript">
+if (typeof jQuery == 'undefined') {
+    document.write(unescape("%3Cscript src='/js/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));
+}
+</script>
+<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
--- includes/_head.php
+++ includes/_head.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link rel="stylesheet" type="text/css" href="/css/cssreset.css">
-    <link rel="stylesheet" type="text/css" href="/css/cssforall.css">
-    <link rel="stylesheet" type="text/css" href="/css/cssindexonly.css">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width">
+    <meta charset="utf8">
+
+<!--     <link rel="stylesheet" type="text/css" href="/css/cssreset.css">
+    <link rel="stylesheet" type="text/css" href="/css/cssforall.css">
+   <link rel="stylesheet" type="text/css" href="/css/cssindexonly.css"> -->
 </head>
