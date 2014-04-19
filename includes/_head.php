fd3a05fb12bd811a9659e10b964f563b7ed7719d
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-18 23:46:08 -0500

    Including custom styles.css

--- includes/_head.php
+++ includes/_head.php
@@ -3,6 +3,7 @@
     <head>
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
     <meta name="viewport" content="width=device-width">
     <meta charset="utf8">
 
@@ -10,3 +11,4 @@
     <link rel="stylesheet" type="text/css" href="/css/cssforall.css">
    <link rel="stylesheet" type="text/css" href="/css/cssindexonly.css"> -->
 </head>
+<body class="container">
