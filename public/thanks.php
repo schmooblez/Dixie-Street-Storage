674e2cb58e016131d55415cfd3b6b645c2d5cf77
Josh Freeman <jdfreeman@satx.rr.com>
2014-04-19 01:49:04 -0500

    Added a thanks response

---
+++ public/thanks.php
@@ -0,0 +1,9 @@
+<?php require "./../includes/_head.php"; ?>
+<body>
+<!--Top logo-->
+<?php require "./../includes/_navigation.php" ?>
+  <h1>Thank you for Contacting Us!</h1>
+  <p>A member of our team will responsd shortly!</p>
+<?php require "./../includes/_footer.php"; ?>
+</body>
+</html>
